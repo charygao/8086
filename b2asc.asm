552638790807d72277301f83977a0819cf8384e9
Pushkar Kurhekar <pshkrh@gmail.com>
2017-08-07 12:52:42 +0530

    BCD To ASCII

---
+++ b2asc.asm
@@ -0,0 +1,21 @@
+.model small
+.stack 100h
+.data
+	n1 db 42h
+	p db ?
+.code
+START:  mov ax,@data
+		mov ds,ax
+		mov al,n1
+		mov ah,00h
+		and al,0f0h
+		rol al,04h
+		mov bl,al
+		mov al,n1
+		and al,00fh
+    add al,30h
+    add bl,30h
+		mov ah,4ch
+		int 21h
+END START
+END
